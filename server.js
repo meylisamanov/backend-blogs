0ba5af3dd797b99743214af47f9eecb523724000
ladyhand <ladyhand8@outlook.com>
2022-03-07 01:13:56 +0000

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes/api');
 const path = require('path');
@@ -7,6 +8,8 @@ require('dotenv').config();
 
 
 const app = express();
+
+app.use(cors())
 
 const port = process.env.PORT || 5000;
 
